96ceb62f555765687836f1b9e47d32e9186b9f41
taiyun-coder <66707554+taiyun-coder@users.noreply.github.com>
2020-06-10 11:32:56 +1000

    Fav
    set and return "name"

---
+++ Fav.java
@@ -0,0 +1,15 @@
+package com.example.contactmanager;
+
+public class Fav {
+    private String _name;
+
+    public  Fav(String name){
+
+        //variables that needed
+        _name = name;
+    }
+
+    public String getfavName(){
+        return _name;
+    }
+}
